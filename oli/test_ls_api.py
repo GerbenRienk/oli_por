9c246ed9a20a00cbaab08915db6d4a3ca2f74131
Gerben Rienk <GerbenRienk@GMail.com>
2018-12-01 09:28:19 +0100

    50%

--- oli/test_ls_api.py
+++ oli/test_ls_api.py
@@ -10,9 +10,8 @@ import base64
 from utils.logmailer import MailThisLogFile
 from utils.dictfile import readDictFile
 from utils.limesurveyrc2api import LimeSurveyRemoteControl2API
-
+from utils.pg_api import ConnToOliDB
 from utils.reporter import Reporter
-#from _operator import itemgetter
 
 def cycle_through_syncs():
     my_report = Reporter()
@@ -20,40 +19,19 @@ def cycle_through_syncs():
     my_report.append_to_report('cycle started at ' + str(start_time))
     # read configuration file for usernames and passwords and other parameters
     config=readDictFile('oli.config')
-    sid = int(config['sid'])
-    print(sid)
+    
+    # create a connection to the postgresql database
+    conn = ConnToOliDB()
+    my_report.append_to_report(conn.init_result)
+    
+    #start the cycling here
     while True:
-        # collecting LimeSurvey data
-        # Make a session, which is a bit of overhead, but the script will be running for hours.
-        api = LimeSurveyRemoteControl2API(config['lsUrl'])
-        session_req = api.sessions.get_session_key(config['lsUser'], config['lsPassword'])
-        session_key = session_req.get('result')
-        api_response = api.surveys.list_surveys(session_key, config['lsUser'])
-        print("surveys: ")
-        all_surveys = api_response['result']
-        
-        for one_survey in all_surveys:
-            if (one_survey['sid'] == sid):
-                print(one_survey['surveyls_title'])
-        # now get all responses of our survey
-        api_response2 = api.responses.export_responses(session_key, sid)
-        responses_b64 = base64.b64decode(api_response2['result'])
-        responses_dict = json.loads(responses_b64)   #this is a dictionary
-        responses_list = responses_dict['responses']
-        
+        # get the resposnes as a list
+        responses_list = read_ls_responses(config)
+        # process the responses one by one
         for one_response in responses_list:
             for response_data in one_response:
-                print(type(one_response[response_data]), one_response[response_data])
-            
-        
-        
-               
-            #print(one_survey['sid'])           
-        # examples of using the api
-        # api.tokens.add_participants(session_key, event_survey_pairs[studysubject_event[1]], participant_data)
-        #participants_req = api.tokens.list_participants(session_key, sid)
-        #participants = participants_req.get('result')
-        
+                print(type(one_response[response_data]), one_response[response_data])    
         
         
         # some book keeping to check if we must continue looping, or break the loop
@@ -72,6 +50,24 @@ def cycle_through_syncs():
     my_report.close_file()
     MailThisLogFile('logs/report.txt')
     
+def read_ls_responses(config):
+    """
+    function to use the ls api and read all responses into a dictionary
+    parameters
+    config is the dictionary with all configuration elements
+    """
+    # collecting LimeSurvey data
+    # Make a session, which is a bit of overhead, but the script will be running for hours.
+    # get the survey id of the one survey we're interested in and cast it into an integer
+    sid = int(config['sid'])
+    api = LimeSurveyRemoteControl2API(config['lsUrl'])
+    session_req = api.sessions.get_session_key(config['lsUser'], config['lsPassword'])
+    session_key = session_req.get('result')
+    # now get all responses of our survey
+    api_response2 = api.responses.export_responses(session_key, sid)
+    responses_b64 = base64.b64decode(api_response2['result'])
+    responses_dict = json.loads(responses_b64)   #this is a dictionary
+    return responses_dict['responses']
     
 if __name__ == '__main__':
     cycle_through_syncs()
--- what_i_did.md
+++ what_i_did.md
@@ -6,3 +6,25 @@
 - copy package config from oli3.1
 - put oli.config in .gitignore by right-clicking on it and choosing Team - Ignore; commit and push and it works (!)
 - put an example oli_example.config next to it, so others understand how it works
+
+- create a db-user for administrative purposes:
+CREATE USER oli WITH LOGIN
+  UNENCRYPTED PASSWORD 'xxx'
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+- create database
+CREATE DATABASE oli_por OWNER oli;
+
+CREATE TABLE ls_responses
+(
+  study_subject_id character varying(32) NOT NULL,
+  study_subject_oid character varying(32) NOT NULL,
+  sid integer, -- survey id, as is used in LimeSurvey...
+  response_id integer,
+  CONSTRAINT pk_study_subject_oc PRIMARY KEY (study_subject_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE ls_responses
+  OWNER TO oli;
+COMMENT ON COLUMN ls_responses.sid IS 'survey id, as is used in LimeSurvey for oli_por this will always be the same: 620620 still it feels like a good idea';
