d127b0b48d6c0a208f2dffa3385a36eab601c672
Gerben Rienk <GerbenRienk@GMail.com>
2018-12-09 10:49:45 +0100

    diagnose connection error

--- oli/test_util_db.py
+++ oli/test_util_db.py
@@ -1,18 +1,27 @@
-
+import psycopg2
+from utils.dictfile import readDictFile
 
 def try_this():
-    v = "O pai é funcionário na escola que a criança "
-    #v= nicode(v)
-    #v.encode('ascii', 'xmlcharrefreplace')
-    print(v.encode(encoding="ascii",errors="xmlcharrefreplace"))
-    txt = "My name is Ståle"
-
-    print(txt.encode(encoding="ascii",errors="backslashreplace"))
-    print(txt.encode(encoding="ascii",errors="ignore"))
-    print(txt.encode(encoding="ascii",errors="namereplace"))
-    print(txt.encode(encoding="ascii",errors="replace"))
-    print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
-    print(txt.encode(encoding="ascii",errors="strict"))
+    conn = ConnToOliDB()
+    print(conn.init_result)
+    
+class ConnToOliDB(object):
+    '''Class for connecting to the postgresql database as defined in oli.config
+    Methods implemented now are read subjects and add subjects '''
+    def __init__(self):
+        'let us create the connection to use multiple times'
+        config=readDictFile('oli.config')
+        conn_string = "host='%s' dbname='%s' user='%s' password='%s' port='%s'" % (config['dbHost'], config['dbName'], config['dbUser'], config['dbPass'], config['dbPort'])
+        self.init_result = ''
+        
+        # get a connection, if a connect cannot be made an exception will be raised here
+        try:
+            self._conn = psycopg2.connect(conn_string)
+            connect_result = 'INFO: class connected to %s, %s as %s on port %s' % (config['dbHost'], config['dbName'], config['dbUser'], config['dbPort'])
+        except psycopg2.OperationalError  as e:
+            connect_result = format(e)
+        
+        self.init_result = connect_result
 
 
 
